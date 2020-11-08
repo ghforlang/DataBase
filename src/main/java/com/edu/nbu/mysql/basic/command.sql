d30ccdfbbf6822952a769bfaa926980dfefd60da
fanwenhao <fanwenhao121@163.com>
2020-11-08 22:46:55 +0800

    20201108 22:30:00

--- src/main/java/com/edu/nbu/mysql/basic/command.sql
+++ src/main/java/com/edu/nbu/mysql/basic/command.sql
@@ -6,4 +6,11 @@ SHOW PROFILES ;
 SHOW PROFILE;
 
 -- 查看系统配置,例如查看log配置
-SHOW VARIABLES LIKE 'log_%'
+SHOW VARIABLES LIKE 'log_%';
+
+-- 查看事务自动提交是否开启
+SELECT @@AUTOCOMMIT;
+SHOW VARIABLES LIKE '%commit%';
+-- 查看缓存大小
+SELECT @@sort_buffer_size;
+SHOW VARIABLES LIKE '%buffer_size%';
