3a2e32f29fa462f14e7ea92a6abc09101e21d257
fanwenhao <fanwenhao121@163.com>
2020-11-10 22:40:48 +0800

    20201110 22:30:00

--- src/main/java/com/edu/nbu/mysql/basic/command.sql
+++ src/main/java/com/edu/nbu/mysql/basic/command.sql
@@ -1,3 +1,61 @@
+-- 登陆 输入密码
+-- mysql -h localhost -u root -p
+
+-- 切换到 test库
+USE test;
+
+-- 查看当前使用的数据库
+SELECT database();
+
+-- 查看当前连接有哪些数据库
+SHOW databases ;
+
+-- 查看当前库有哪些表
+SHOW TABLES;
+
+-- 查看简单描述表结构
+DESC user;
+-- 查看user表的建表语句
+SHOW CREATE TABLE user;
+SHOW CREATE TABLE user \G;
+-- 复制表结构
+CREATE TABLE user1 LIKE  user;
+-- 复制表数据
+INSERT INTO TABLE user1 SELECT * FROM user;
+-- 查看test库user表中列的注释信息
+SELECT * FROM information_schema.columns
+WHERE table_schema= 'test'
+AND table_name ='user';
+
+-- 查看test库user表执行列信息
+SELECT column_name as '列',column_type as '类型',column_comment as '注释'
+FROM information_schema.columns
+WHERE table_schema = 'test'
+AND table_name = 'user';
+
+-- 查看表注释
+SELECT table_name as '表名',table_comment as  '注释'
+FROM information_schema.tables
+WHERE table_schema = 'test'
+  AND table_name = 'user';
+
+-- 更新表结构
+ALTER TABLE user ADD INDEX `idx_name`(`name`) COMMENT '名称索引';
+ALTER TABLE user ADD COLUMN `age` int(10) UNSIGNED NOT NULL COMMENT '年龄';
+
+-- 获取表的write锁定 MyISAM；
+LOCK TABLE `user` write;
+-- 解锁
+UNLOCK TABLES;
+-- 获取表的read锁定 MyISAM
+LOCK TABLE  `user` read;
+-- 与上一命令不同，相关操作只会影响到local;
+LOCK TABLE `user` READ LOCAL;
+
+-- table_locks_waited和table_locks_immediate状态变量来分析系统上的表锁定争夺
+-- Table_locks_waited的值比较高，则说明存在着较严重的表级锁争用情况
+show status like 'table%';
+
 -- 查询连接数
 SHOW PROCESSLIST ;
 
@@ -14,3 +72,6 @@ SHOW VARIABLES LIKE '%commit%';
 -- 查看缓存大小
 SELECT @@sort_buffer_size;
 SHOW VARIABLES LIKE '%buffer_size%';
+
+
+
